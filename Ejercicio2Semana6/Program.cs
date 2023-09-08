e5ffe7e72ab37605d6d4ee7e1a5dd4bb4eab3c9a
sujey22 <da0089032022@unab.edu.sv>
2023-09-07 19:17:12 -0600

    Create Program.cs

---
+++ Ejercicio2Semana6/Program.cs
@@ -0,0 +1,13 @@
+﻿//Ejercicio 2
+//La operación completa incluye crear un origen de datos, definir la expresión de
+//consulta y ejecutar la consulta en una instrucción foreach.
+
+int[] Score = { 90, 71, 82, 93, 75, 82 };
+
+var Query = (from x in Score where x > 80 select x);
+
+var descend = Query.OrderByDescending(x => x.ToString());
+
+Console.WriteLine(string.Join(", ", descend));
+
+Console.WriteLine("Hecho por Merlin Sujey Duarte");
